278569616ae3037298bc2b767f1d2b8e2d32b0d5
Nils <nilsbaumgartner1994@gmail.com>
2024-06-01 14:25:21 +0200

    remove unstable settings

--- app/app/(app)/_layout.tsx
+++ app/app/(app)/_layout.tsx
@@ -3,11 +3,6 @@ import React from 'react';
 import {Text} from 'react-native'
 import {Drawer} from "expo-router/drawer";
 
-export const unstable_settings = {
-	// Ensure that reloading on `/modal` keeps a back button present.
-	initialRouteName: 'index',
-};
-
 export default function AppLayout() {
 	const pathName = usePathname();
 	const rootNavigationState = useRootNavigationState();
